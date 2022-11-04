b914717cf7187361f477f0fbe317cbbd5be2bd97
scv718 <skfnxh718@naver.com>
2022-11-04 17:19:37 +0900

    결제페이지 및 결제 테이블 결제 완료

---
+++ src/main/java/com/project/order/impl/OrderDAOmybatis.java
@@ -0,0 +1,24 @@
+package com.project.order.impl;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.project.order.OrderVO;
+import com.project.review.ReviewVO;
+
+@Repository
+public class OrderDAOmybatis {
+	
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public void insertOrder(OrderVO vo) {
+		mybatis.insert("OrderDAO.insertOrder", vo);
+		
+	}
+
+
+}
